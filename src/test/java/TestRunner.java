21970b16476270fa25e8ad9c20f7dc08b44e0392
Gabrieltendong <42476845+Gabrieltendong@users.noreply.github.com>
2021-06-10 11:48:22 +0100

    autmation bug 1294

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,4 +1,14 @@
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-
-public class TestRunner {
+@CucumberOptions(
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports/cucumber.html",
+                "json:target/cucumber-reports/cucumber.json",
+        },
+        features = {"src/test/resources/features"},
+        glue = {"StepDefinitions"}
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
 }
